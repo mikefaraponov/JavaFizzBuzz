4afc35de55d69b0693b4d2aa081a8f4898a241fa
mikefaraponov <java.math.random@bk.ru>
2016-05-28 19:12:44 +0300

    Docs added

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,23 +5,49 @@ public final class FizzBuzz {
     private final int begin;
     private final int end;
 
+    /**
+     * begin setter
+     * @return value of begin instance variable
+     */
     public int getBegin() {
         return begin;
     }
 
+    /**
+     * end setter
+     * @return value of end instance variable
+     */
     public int getEnd() {
         return end;
     }
 
+    /**
+     * FizzBuzz constructor
+     * @param begin start value of int range
+     * @param end last value of int range
+     */
     private FizzBuzz(int begin, int end) {
         this.begin = begin;
         this.end = end;
     }
 
+    /**
+     * Factory which creates FizzBuzz objects
+     * @param begin
+     * @param end
+     * @return
+     */
     public static FizzBuzz of(int begin, int end){
         return new FizzBuzz(begin, end);
     }
 
+    /**
+     * FizzBuzz converter
+     * @param n input number
+     * @return string with
+     * Fizz if number is divided by 3, Buzz if number is divided by 5, FizzBuzz if number is divided by 15
+     * or current number
+     */
     public static String toFizzBuzz(int n){
         return n % 15 == 0? "FizzBuzz":
                n % 3 == 0? "Fizz":
@@ -29,6 +55,9 @@ public final class FizzBuzz {
                Integer.toString(n);
     }
 
+    /**
+     * @return stream with strings which was converted from IntStream by toFizzBuzz method
+     */
     public Stream<String> getFizzBuzzStream(){
         return IntStream.rangeClosed(begin, end)
                 .mapToObj(FizzBuzz::toFizzBuzz);
